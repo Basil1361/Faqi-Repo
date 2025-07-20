ccaebd987534738f3137dbbdab48d6532cdf4661
Basil1361 <trainerbasil69@gmail.com>
2025-07-20 22:53:54 +0800

    initial compile

---
+++ Jia Jie/TypeScript/Tuples/Tuples.ts
@@ -0,0 +1,10 @@
+// Assigning fixed elements
+// destructuring in an array
+
+var myTuples : [string, number] = ["Hello", 12]
+    let [first, second] = myTuples 
+console.log(first)
+console.log(second)
+//or 
+// console.log(myTuples[0])
+// console.log(myTuples[1])
