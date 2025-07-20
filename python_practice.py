11a52d586e38bd8efec9d4fcce1bbc1dac2bf717
fqhhamn <faqihahamin00@gmail.com>
2025-07-21 01:14:32 +0800

    20/7/25

---
+++ python_practice.py
@@ -0,0 +1,6 @@
+first_name = "Max"
+middle_name = "Emilian"
+last_name = "Verstappen"
+full_name = first_name + " " + middle_name + " " + last_name
+print("Hello" + " " +full_name)
+#print(type(name))
