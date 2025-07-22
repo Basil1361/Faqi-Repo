cad775a4c365e33ee33a4c4462ffd5920edd488e
fqhhamn <faqihahamin00@gmail.com>
2025-07-22 15:56:16 +0800

    variables

--- python_practice.py
+++ python_practice.py
@@ -1,6 +1,19 @@
-first_name = "Max"
-middle_name = "Emilian"
-last_name = "Verstappen"
-full_name = first_name + " " + middle_name + " " + last_name
-print("Hello" + " " +full_name)
+#first_name = "Max"
+#middle_name = "Emilian"
+#last_name = "Verstappen"
+#full_name = first_name + " " + middle_name + " " + last_name
+#print("Hello" + " " +full_name)
 #print(type(name))
+
+#age = 15
+#age += 1
+#print("Your age is: " + str(age))
+#print(type(age))
+
+#height = 193.5
+#print("Your height is: " + str(height) + "cm")
+#print(type(height))
+
+#cat = False
+#print("Are you a feline: " + str(cat))
+#print(type(cat))
