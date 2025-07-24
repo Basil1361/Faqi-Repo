2d652547d900c44b5c2fa33905ae0dd01004bac5
fqhhamn <faqihahamin00@gmail.com>
2025-07-24 22:01:35 +0800

    24/7

---
+++ Python/multiple_assignment.py
@@ -0,0 +1,19 @@
+# name = "Jason"
+# age = 22
+# attractive = True
+
+name, age, attractive = "Jason", 22, True
+print(name)
+print(age)
+print(attractive)
+
+#Mior = 52
+#Farah = 52
+#Jayden = 52
+#Abhijith = 52
+
+Mior = Farah = Jayden = Abhijith = 52
+print(Mior)
+print(Farah)
+print(Jayden)
+print(Abhijith)
---
+++ Python/string_methods.py
@@ -0,0 +1,12 @@
+name = "astroboy"
+
+#print(len(name))
+#print(name.find("s"))
+#print(name.capitalize())
+#print(name.upper())
+#print(name.lower())
+#print(name.isdigit())
+#print(name.isalpha())
+#print(name.count("b"))
+#print(name.replace("o", "i"))
+#print(name*9)
